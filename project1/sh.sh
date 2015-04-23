57fcef0554491749c83cd60779ae0f98650613e1
xcyivan <xiacheyunivan@gmail.com>
2015-04-22 21:57:25 -0700

    ivan Apr 22th

--- project1/sh.sh
+++ project1/sh.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # (cat) < sh.sh && cat < sh.sh
 
-(exec cat) < sh.sh
+#(exec cat) < sh.sh
 
 
 
@@ -10,7 +10,12 @@
 
 # ls -l   && ls -l  ./a|| ls
 
- ls |  grep s  && exec ls
+# ls |  grep s  && exec ls
+
+# #exec echo left && exec echo right
+#ls | grep s && exec echo shut
+#echo hello
 
 
-
+exec ls | grep s
+echo hello
